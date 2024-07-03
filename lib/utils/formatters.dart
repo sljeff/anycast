0a4d9815f7e3140df0e401dc906efe591e10ceb8
sljeff <kindjeff.com@gmail.com>
2024-07-03 19:09:10 +0800

    fix(timeago): use short en

--- lib/utils/formatters.dart
+++ lib/utils/formatters.dart
@@ -6,12 +6,13 @@ import 'package:sanitize_html/sanitize_html.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:url_launcher/url_launcher.dart';
 
+// my_custom_messages.dart
 String formatDatetime(int ts) {
   var dt = DateTime.fromMillisecondsSinceEpoch(ts);
   // in a week: use timeago; in this year: use month and day; else: use yyyy-mm-dd
   var now = DateTime.now();
   if (dt.isAfter(now.subtract(const Duration(days: 7)))) {
-    return timeago.format(dt);
+    return timeago.format(dt, locale: 'en_short');
   } else if (dt.year == now.year) {
     return '${dt.month}-${dt.day}';
   } else {
